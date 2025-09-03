12c675fa7b7b7ce7066f6029b755c473a681cead
f7475260-png <f7475260@gmail.com>
2025-09-03 15:42:54 +0200

    Create seed.js

---
+++ seed.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const faker = require("faker");
+
+mongoose.connect("mongodb://127.0.0.1:27017/trendbattle", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const DuelSchema = new mongoose.Schema({
+  title: String,
+  optionA: String,
+  optionB: String,
+  votesA: { type: Number, default: 0 },
+  votesB: { type: Number, default: 0 }
+});
+
+const Duel = mongoose.model("Duel", DuelSchema);
+
+(async () => {
+  console.log("🚀 Seeding 1,000,000 duels...");
+  let bulk = [];
+  for (let i = 0; i < 1000000; i++) {
+    bulk.push({
+      title: faker.lorem.words(3),
+      optionA: `https://picsum.photos/400?random=${i}`,
+      optionB: `https://picsum.photos/400?random=${i + 1000000}`
+    });
+
+    if (bulk.length === 10000) {
+      await Duel.insertMany(bulk);
+      bulk = [];
+      console.log(`${i} duels insérés`);
+    }
+  }
+  console.log("✅ Done");
+  process.exit();
+})();
